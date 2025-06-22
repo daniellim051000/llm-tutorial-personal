189f9dc63ed0091ec3e6e29cd4c8c2146afd45b1
Daniel Lim <daniellim0510@Daniels-MacBook-Pro-2.local>
2025-06-22 15:27:00 +0800

    learn simple llm with langchain

---
+++ simple-llm/main.py
@@ -0,0 +1,64 @@
+import getpass
+import os
+
+try:
+    # load environment variables from .env file (requires `python-dotenv`)
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except ImportError:
+    pass
+
+os.environ["LANGSMITH_TRACING"] = "true"
+if "LANGSMITH_API_KEY" not in os.environ:
+    os.environ["LANGSMITH_API_KEY"] = getpass.getpass(
+        prompt="Enter your LangSmith API key (optional): "
+    )
+if "LANGSMITH_PROJECT" not in os.environ:
+    os.environ["LANGSMITH_PROJECT"] = getpass.getpass(
+        prompt='Enter your LangSmith Project Name (default = "default"): '
+    )
+    if not os.environ.get("LANGSMITH_PROJECT"):
+        os.environ["LANGSMITH_PROJECT"] = "default"
+
+if not os.environ.get("AZURE_OPENAI_API_KEY"):
+  os.environ["AZURE_OPENAI_API_KEY"] = getpass.getpass("Enter API key for Azure: ")
+
+from langchain_openai import AzureChatOpenAI
+
+model = AzureChatOpenAI(
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    azure_deployment=os.environ["AZURE_OPENAI_DEPLOYMENT_NAME"],
+    openai_api_version=os.environ["AZURE_OPENAI_API_VERSION"],
+)
+
+# from langchain_core.messages import HumanMessage, SystemMessage
+
+# messages = [
+#     SystemMessage("Translate the following from English into Italian"),
+#     HumanMessage("hi!"),
+# ]
+
+# model.invoke(messages)
+
+# model.invoke("Hello")
+
+# model.invoke([{"role": "user", "content": "Hello"}])
+
+# model.invoke([HumanMessage("Hello")])
+
+
+from langchain_core.prompts import ChatPromptTemplate
+
+system_template = "Translate the following from English into {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "Chinese", "text": "hi!"})
+
+prompt.to_messages()
+
+response = model.invoke(prompt)
+print(response.content)
